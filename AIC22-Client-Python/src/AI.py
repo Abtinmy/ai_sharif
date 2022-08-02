a78844cfe606b58549e76e8233d0ab68173d0df2
ghazalrafiei <gh.rafiei99@gmail.com>
2022-08-02 18:29:50 +0430

    Add thief movement

--- AIC22-Client-Python/src/AI.py
+++ AIC22-Client-Python/src/AI.py
@@ -36,11 +36,29 @@ class AI:
                 pc += 1
         return pc
 
+    def thieves_count(self, node_id, view: GameView) -> int:
+        tc = 0
+        for vu in view.visible_agents:
+            if not vu.is_dead and vu.node_id == node_id and vu.agent_type == hide_and_seek_pb2.AgentType.THIEF:
+                tc += 1
+        return tc
+
     def pr_police(self, node_id, view: GameView) -> float:
         pr = 0.001
         for pth in view.config.graph.paths:
-            if pth.second_node_id == node_id:  # destination is node_id
+            if pth.second_node_id == node_id:  # destination is current node
                 pr += self.police_count(pth.first_node_id, view) * \
+                    self.get_degree(pth.first_node_id, view)
+        return pr
+
+    def pr_theives(self, node_id, view: GameView) -> float:
+        pr = 1
+        if GameView.turn.turnNumber in view.turnSettings.visibleTurns:
+            return pr
+
+        for pth in view.config.graph.paths:
+            if pth.second_node_id == node_id:  # destination is current node
+                pr += self.thieves_count(pth.first_node_id, view) * \
                     self.get_degree(pth.first_node_id, view)
         return pr
 
@@ -50,8 +68,8 @@ class AI:
         # for m in range(len(view.visible_agents)):
         #     message = message  + '0'
         # self.phone.send_message(message)
+        h = {}      # h(next) = cost * (prob. Of polices)
         current_node = view.viewer.node_id
-        h = {}
         for pth in view.config.graph.paths:
             if pth.first_node_id == current_node:
                 adj_id = pth.first_node_id
@@ -63,17 +81,35 @@ class AI:
         move_to = -1
         for adj_id in h.keys():
             if h[adj_id] < min_h:
+                min_h = h[adj_id]
                 move_to = adj_id
 
-        if h[adj_id] != INF:
-            return adj_id
-
+        if min_h != INF:
+            return move_to
         else:
-            # means all degree 1 --> I think it's impossible OR price > current balance
-            return -1  #Means stay there. how is it handled?!
+            return current_node  # Stay?
 
     def police_move_ai(self, view: GameView) -> int:
-        # write your code here
-        
-        # self.phone.send_message('00101001')
-        return 1
+        # write your code here current_node = view.viewer.node_id
+        # h(next) = cost * (pr. Of polices)*(1-pr. Of thieves)/d(next)
+        h = {}
+        current_node = view.viewer.node_id
+        for pth in view.config.graph.paths:
+            if pth.first_node_id == current_node:
+                adj_id = pth.first_node_id
+                h[adj_id] = INF
+                if self.get_degree(adj_id, view) != 1 and view.balance > pth.price:
+                    h[adj_id] = pth.price * self.pr_police(adj_id, view) \
+                        * (1-self.pr_theives(adj_id, view)) / self.get_degree(adj_id, view)
+
+        min_h = INF
+        move_to = -1
+        for adj_id in h.keys():
+            if h[adj_id] < min_h:
+                min_h = h[adj_id]
+                move_to = adj_id
+
+        if min_h != INF:
+            return move_to
+        else:
+            return current_node  # Stay
