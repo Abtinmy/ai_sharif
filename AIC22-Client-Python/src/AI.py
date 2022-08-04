e1cf97465a66a4572c925338b39d742b0b9e497d
ghazalrafiei <gh.rafiei99@gmail.com>
2022-08-04 20:24:00 +0430

    Add simpler heuristic

--- AIC22-Client-Python/src/AI.py
+++ AIC22-Client-Python/src/AI.py
@@ -1,11 +1,12 @@
+import random
+import math
 from src.client import GameClient
 from src.model import GameView
 from src import hide_and_seek_pb2
-import random
 
 
 INF = float('inf')
-PR_STAY = 3
+PR_STAY = 10
 
 
 def write(txt):
@@ -20,21 +21,21 @@ def convert_paths_to_adj(paths, n):
     inf = float('inf')
     adj = [[inf for j in range(n+1)] for i in range(n+1)]
 
-    max_price = float('-inf')
+    min_price = inf
     for path in paths:
         adj[path.first_node_id][path.second_node_id] = path.price
         adj[path.second_node_id][path.first_node_id] = path.price
-        if path.price > max_price:
-            max_price = path.price
+        if path.price < min_price:
+            min_price = path.price
 
     for i in range(n+1):
         adj[i][i] = 0
 
-    # Price normalization
-    if max_price != 0:
+    # Price normalization: All/Min
+    if min_price != 0:
         for i in range(n+1):
             for j in range(n+1):
-                adj[i][j] /= max_price
+                adj[i][j] /= min_price
 
     write(str(adj))
     return adj
@@ -55,7 +56,7 @@ def floyd_warshall(paths, n):
 
 
 def get_thief_starting_node(view: GameView) -> int:
-    return random.randint(2, len(view.config.graph.nodes)+1)
+    return random.randint(2, len(view.config.graph.nodes))
 
     # i = int(len(view.config.graph.nodes)/len(AllThieves?!))
     # st_node = random.randint(i*view.id, i*view.id+i)
@@ -104,11 +105,11 @@ class AI:
         if self.degrees is None:
             self.degrees = self.get_degrees(view)
 
-        pr = 0.001
+        pr = 1
 
         nodes_count = len(view.config.graph.nodes)
         for adj_id in range(1, nodes_count+1):
-            if self.cost[node_id][adj_id]:
+            if self.cost[node_id][adj_id] != INF:
                 pr += self.police_count(adj_id, view) / \
                     self.degrees[adj_id]
         return pr
@@ -121,7 +122,7 @@ class AI:
 
         nodes_count = len(view.config.graph.nodes)
         for adj_id in range(1, nodes_count+1):
-            if self.cost[node_id][adj_id]:
+            if self.cost[node_id][adj_id] != INF:
                 pr += self.thieves_count(adj_id, view) / \
                     self.degrees[adj_id]
         return pr
@@ -139,32 +140,43 @@ class AI:
         # self.phone.send_message(message)
         h = {}      # h(next) = cost * (prob. Of polices)
         current_node = view.viewer.node_id
+        current_threat = self.pr_police(current_node, view)
         for adj_id in range(1, nodes_count+1):
+            h[adj_id] = INF
             # write(f"{current_node} !!! {adj_id}")
             if self.cost[current_node][adj_id] != INF:
-                h[adj_id] = INF
-                if self.degrees[adj_id] != 1 and view.balance > self.cost[current_node][adj_id]:
-                    h[adj_id] = (self.cost[current_node][adj_id]) * \
-                        self.pr_police(adj_id, view)
+                h[adj_id] = self.pr_police(adj_id, view)
+            #     h[adj_id] = INF
+            #     if self.degrees[adj_id] != 1 and view.balance > self.cost[current_node][adj_id]:
+            #         h[adj_id] = (math.sqrt(self.cost[current_node][adj_id])) * \
+            #             self.pr_police(adj_id, view)
 
         min_h = INF
-        move_to = -1
+        move_to = current_node
+        # for adj_id in h.keys():
+        #     # be ehtemale 1/PR-1 bemoone sare jash
+        #     if h[adj_id] == 0 and random.randint(0, PR_STAY-1):
+        #         continue
+        #     if h[adj_id] < min_h:
+        #         # age mosavi shod, ba ehtemale 1/2 bere ya bemoone
+        #         if h[adj_id] == min_h and random.randint(0, 1):
+        #             min_h = h[adj_id]
+        #             move_to = adj_id
+        #         else:
+        #             min_h = h[adj_id]
+        #             move_to = adj_id
+        min_threat = current_threat
         for adj_id in h.keys():
-            # be ehtemale 1/PR-1 bemoone sare jash
-            if h[adj_id] == 0 and random.randint(0, PR_STAY-1):
-                continue
-            if h[adj_id] < min_h:
-                # age mosavi shod, ba ehtemale 1/2 bere ya bemoone
-                if h[adj_id] == min_h and random.randint(0, 1):
-                    min_h = h[adj_id]
-                    move_to = adj_id
-                else:
-                    min_h = h[adj_id]
-                    move_to = adj_id
+            # if h[adj_id] <= min_threat:
+            if h[adj_id] != INF and (h[adj_id] < min_threat or (h[adj_id] == min_threat and random.randint(0, 1))):
+
+                min_threat = h[adj_id]
+                move_to = adj_id
 
         write("Thief: "+str(h))
 
-        if min_h != INF:
+        # if min_h != INF:
+        if True:
             write("Thief with id " + str(view.viewer.id) + " in node " +
                   str(current_node) + " move to " + str(move_to))
             return move_to
@@ -183,32 +195,40 @@ class AI:
 
         h = {}  # h(x) = (cost * pr_police) / (pr_thieves * degree)
         current_node = view.viewer.node_id
-
+        current_threat = self.pr_theives(current_node, view)
         for adj_id in range(1, nodes_count+1):
+            h[adj_id] = INF
             if self.cost[current_node][adj_id] != INF:
-                h[adj_id] = INF
-                if self.degrees[adj_id] != 1 and view.balance > self.cost[current_node][adj_id]:
-                    h[adj_id] = (self.cost[current_node][adj_id]) * self.pr_police(adj_id, view) \
-                        / (self.pr_theives(adj_id, view) * self.degrees[adj_id])
+                h[adj_id] = self.pr_theives(adj_id, view)
+                # and self.degree == 1
+                # if view.balance > self.cost[current_node][adj_id]:
+                #     h[adj_id] = (math.sqrt(self.cost[current_node][adj_id])) * self.pr_police(
+                #         adj_id, view) / self.pr_theives(adj_id, view)  # * self.degrees[adj_id])
 
         min_h = INF
-        move_to = -1
+        move_to = current_node
+        # for adj_id in h.keys():
+        #     # be ehtemale 1/PR bemoone sare jash
+        #     if h[adj_id] == 0 and random.randint(0, PR_STAY-1):
+        #         continue
+        #     if h[adj_id] < min_h:
+        #         # age mosavi shod, ba ehtemale 1/2 bere ya bemoone
+        #         if h[adj_id] == min_h and random.randint(0, 1):
+        #             min_h = h[adj_id]
+        #             move_to = adj_id
+        #         else:
+        #             min_h = h[adj_id]
+        #             move_to = adj_id
+        max_threat = current_threat
         for adj_id in h.keys():
-            # be ehtemale 1/PR bemoone sare jash
-            if h[adj_id] == 0 and random.randint(0, PR_STAY-1):
-                continue
-            if h[adj_id] < min_h:
-                # age mosavi shod, ba ehtemale 1/2 bere ya bemoone
-                if h[adj_id] == min_h and random.randint(0, 1):
-                    min_h = h[adj_id]
-                    move_to = adj_id
-                else:
-                    min_h = h[adj_id]
-                    move_to = adj_id
+            if h[adj_id] != INF and (h[adj_id] > max_threat or (h[adj_id] == max_threat and random.randint(0, 1))):
+                move_to = adj_id
+                max_threat = h[adj_id]
 
         write("Police: " + str(h))
 
-        if min_h != INF:
+        # if min_h != INF:
+        if True:
             write("Police with id " + str(view.viewer.id) + " in node " +
                   str(current_node) + " move to " + str(move_to))
             return move_to
