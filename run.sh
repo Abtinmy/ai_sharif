18fc2c27032569c8e73df754cac4d66a1c1bc7d2
ghazalrafiei <gh.rafiei99@gmail.com>
2022-08-04 22:52:19 +0430

    Add random opponent + update run.sh

--- run.sh
+++ run.sh
@@ -4,10 +4,18 @@ echo '===== logs deleted ====='
 
 cd ./AIC22-Client-Python/
 rm -rf src/dist/ src/build/
-echo '===== folder dist and build deteled ====='
 pyinstaller --onefile src/client.py
+echo '===== opponent 1 ready ====='
 
-echo '===== pyinstaller client done ====='
+cd ../AIC22-Client-Python2/
+rm -rf src/dist/ src/build/
+pyinstaller --onefile src/client.py
+echo '===== opponent 2 ready ====='
+
 cd ../server
-java -jar hideandseek-0.0.7.jar --first-team=../AIC22-Client-Python/dist/client --second-team=../AIC22-Client-Python/dist/client resources/game.yml resources/map.json
+# java -jar hideandseek-0.0.7.jar --first-team=../AIC22-Client-Python/dist/client --second-team=../AIC22-Client-Python/dist/client resources/game.yml resources/map.json
+
+# Play with random oponent
+java -jar hideandseek-0.0.7.jar --first-team=../AIC22-Client-Python/dist/client --second-team=../AIC22-Client-Python2/dist/client resources/game.yml resources/map.json
+
 
